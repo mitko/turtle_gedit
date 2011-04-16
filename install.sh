5af35cf7cb9bc20320a8d0c491394f7695bb2c3b
Dimitar Simeonov <simeonov.dimitar@gmail.com>
2011-04-16 19:47:09 -0400

    adding the stuff

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo cp n3.lang /usr/share/gtksourceview-2.0/language-specs/n3.lang
+sudo cp n3.xml /usr/share/mime/packages/n3.xml
+cd /usr/share/
+sudo update-mime-database mime
+
+
+
