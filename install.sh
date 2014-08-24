4897b85e2e4414eacadc58efb565368f3d000d9d
Ronald Muller <rmuller@xiam.nl>
2014-08-24 11:17:31 +0200

    Added README and updated installation process

--- install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-sudo cp n3.lang /usr/share/gtksourceview-2.0/language-specs/n3.lang
-sudo cp n3.xml /usr/share/mime/packages/n3.xml
-cd /usr/share/
-sudo update-mime-database mime
-
-
-
